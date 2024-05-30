591759e46cb7d6e56e773f88333f7916a362f858
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2024-05-30 07:58:21 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Valores Chave
+
+SERVER_PORT -> 8888
+SERVER_HOST -> node-c
+NGINX_PORT -> 9999
+NGINX_HOST -> nginx-c
---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+const app = express();
+import mysql from 'mysql';
+
+
+const config = {
+    host: 'mydb',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const connection = mysql.createConnection(config);
+
+const FIND_PEOPLE_NAMES = `SELECT name from people`;
+
+app.get('/', async (req, res) => {
+    try {
+        const INSERT_NAME = `INSERT INTO people(name) values('Gabriel-${new Date().getTime()}')`;
+        connection.query(INSERT_NAME);
+        connection.query(FIND_PEOPLE_NAMES, (err, rows) => {
+            res.status(200).send(`
+                <h1>Full Cycle Rocks!</h1>
+                <ul>
+                    ${rows.map(row => `<li>${row.name}</li>`).join('')}
+                </ul>
+            `);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.listen(8888, () => {
+    console.log(`Express listening on port ${8888}`);
+});
